9a2575548e7349c6e95e1db43b95dcc586129390
kovaciardey <andrei.kovaci@gmail.com>
2020-05-12 20:44:57 +0100

    Added Colours to the texture
    Added coloured texture generated from the noise map
    Refactored the texture generation into it's own separate class

--- procedural-landmass-generation/Assets/Scripts/MapGenerator.cs
+++ procedural-landmass-generation/Assets/Scripts/MapGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode {NoiseMap, ColorMap};
+    public DrawMode drawMode;
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -12,17 +15,38 @@ public class MapGenerator : MonoBehaviour
     [Range(0, 1)]
     public float persistance;
     public float lacunarity;
-    
+
     public int seed;
     public Vector2 offset;
 
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public void GenerateMap() {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                float currentHeight = noiseMap[x, y];
+
+                for (int i = 0; i < regions.Length; i++) {
+                    if (currentHeight <= regions[i].height) {
+                        colourMap[y * mapWidth + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
         MapDisplay display = FindObjectOfType<MapDisplay> ();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap) {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        } else if (drawMode == DrawMode.ColorMap) {
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(colourMap, mapWidth, mapHeight));
+        }
     }
 
     void OnValidate() {
@@ -42,4 +66,11 @@ public class MapGenerator : MonoBehaviour
             octaves = 0;
         }
     }
+}
+
+[System.Serializable]
+public struct TerrainType {
+    public string name;
+    public float height;
+    public Color color;
 }
